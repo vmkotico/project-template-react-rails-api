87c3a53b65690f055b8b4586ec48d30951202a93
ChristianMFlatiron <chrisrmonteverde@gmail.com>
2021-12-09 00:24:14 -0800

    CMTestRenderAll

--- client/src/Components/SpellsCard.js
+++ client/src/Components/SpellsCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./index.css";
 
-function SpellCard({ spell: { name, description, image, bio, id } }) {
+function SpellCard({ spell: { name, image, style, effect, range, id } }) {
   return (
     <div class="card">
       <div>{name}</div>
       <br></br>
-      <div>{description}</div>
+      <div>{style}</div>
       <br></br>
-      <div>{bio}</div>
+      <div>{effect}</div>
+      <br></br>
+      <div>{range}</div>
       <br></br>
       <img src={image} />
     </div>
