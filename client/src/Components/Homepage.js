cc5ee4e428a5b593ed6934fe20980e416cff3f95
Vida May Kotico <vmkotico@yahoo.com>
2021-12-09 17:26:04 -0800

    rebase

--- client/src/Components/Homepage.js
+++ client/src/Components/Homepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./Navbar";
-import Finalfantasy from "./assets/finalfantasy.mp4"
+
 // import { useHistory } from "react-router-dom";
 
 // const history = useHistory();
@@ -8,16 +8,7 @@ import Finalfantasy from "./assets/finalfantasy.mp4"
 function Homepage() {
   return (
     <div>
-      <video autoPlay muted controls  
-      style = {{ 
-        position: "absolute",
-        width: "50%",
-        height: "50%", 
-        left: "25%",
-        right: "50%"
-      }}>
-        <source src = {Finalfantasy} type = "video/mp4" start = {0.40} />
-      </video>
+      
     </div>
   );
 }
